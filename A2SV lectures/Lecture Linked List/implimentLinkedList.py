a72c71bf5d65ffef0ccfe7e2f1e4e9eccc4ccb0f
Haile Adugna <hordofahaile7@gmail.com>
2023-02-20 15:05:52 +0300

    linked list

--- A2SV lectures/Lecture Linked List/implimentLinkedList.py
+++ A2SV lectures/Lecture Linked List/implimentLinkedList.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None # Type: Node
+
+    def traverse(self, head) -> None:
+        currentNode = head
+        while currentNode:
+            print(currentNode.val)
+            currentNode = currentNode.next
