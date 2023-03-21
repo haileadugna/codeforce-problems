660033e14611d7fb19f0f7169638ccc6852040bc
Haile Adugna <hordofahaile7@gmail.com>
2023-03-21 23:42:29 +0300

    less or equal

--- A2SV practice/C_Less_or_Equal.py
+++ A2SV practice/C_Less_or_Equal.py
@@ -1,11 +1,17 @@
 n, k = map(int, input().split())
 nums= list(map(int, input().split()))
-
 nums.sort()
-res = -1
-if k == n  :
-    res = max(nums)
-elif nums[k-1] != nums[k]:
-    res = nums[k - 1] 
-print(res)
-# for i in range(k):
+if k == 0:
+    ans = nums[0] - 1
+else:
+    ans = nums[k -1]
+
+cnt = 0
+for i in range(len(nums)):
+    if nums[i] <= ans:
+        cnt += 1
+
+if cnt != k or not (1 <= ans and ans <= 1000**3):
+    ans = -1
+print(ans)
+
