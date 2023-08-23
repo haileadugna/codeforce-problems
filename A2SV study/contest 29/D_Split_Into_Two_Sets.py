65bdd7f4fd659dcd9a2cdf84d31d9aa1c7647030
haileadugna <hordofahaile7@gmail.com>
2023-08-23 10:58:30 +0300

    edit

--- A2SV study/contest 29/D_Split_Into_Two_Sets.py
+++ A2SV study/contest 29/D_Split_Into_Two_Sets.py
@@ -19,11 +19,15 @@ for _ in range(test):
             list2.add(nums[i][0])
             list2.add(nums[i][1])
 
-        else:
+        elif nums[i][0] not in list1 or nums[i][1] not in list1:
             list1.add(nums[i][0])
             list1.add(nums[i][1])
 
-    print(list1, 'set1')
+        else:
+            print("NO")
+            break
+
+    # print(list1, 'set1')
     if len(list1) == n:
         print('YES')
     else:
